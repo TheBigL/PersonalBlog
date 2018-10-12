e3c411aa74737db5387b89e1fd01117f6c8720bd
TheBigL <lmohamed1@studentmail.nait.ca>
2018-10-12 15:56:55 -0600

    Adding Indexs

--- db/migrate/20181012195650_add_role_tableto_database.rb
+++ db/migrate/20181012195650_add_role_tableto_database.rb
@@ -1,10 +1,9 @@
 class AddRoleTabletoDatabase < ActiveRecord::Migration[5.2]
   def change
     create_table :roles do |t|
-      t.int :roleId
+      t.integer :roleId
       t.string :roleName
     end
-
     remove_column :users, :role
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_08_165917) do
+ActiveRecord::Schema.define(version: 2018_10_12_195650) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -78,6 +78,11 @@ ActiveRecord::Schema.define(version: 2018_08_08_165917) do
     t.index ["post_id"], name: "index_posts_on_post_id"
   end
 
+  create_table "roles", force: :cascade do |t|
+    t.integer "roleId"
+    t.string "roleName"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
     t.string "email", default: "", null: false
@@ -93,7 +98,6 @@ ActiveRecord::Schema.define(version: 2018_08_08_165917) do
     t.integer "failed_attempts", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
