21b13a861d259032ddeed2e792c2248f2a573b38
TheBigL <lmohamed1@studentmail.nait.ca>
2019-02-05 11:17:09 -0700

    Started Adding Images

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   devise_for :users, controllers: { registrations: "users/registrations", sessions: 'users/sessions'}
   resources :users do
-    get :confirm_token
+    get :confirmation_token
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts do
