ee9d9a8421c99dcbf759573fd8b71b8c937fdf26
TheBigL <lmohamed1@studentmail.nait.ca>
2019-02-15 23:18:27 -0700

    Removed Devise::Mailer dependency

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,4 @@
-class UserMailer < Devise::Mailer
+class UserMailer < ActionMailer::Base
 
   helper :application # gives access to all helpers defined within `application_helper`.
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
