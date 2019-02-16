ebaa80e4224eea0aed2be4edf40f053259437283
TheBigL <lmohamed1@studentmail.nait.ca>
2019-02-15 23:25:43 -0700

    Dependent on Application Mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,11 @@
-class UserMailer < ActionMailer::Base
-
-  helper :application # gives access to all helpers defined within `application_helper`.
-  include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
-  default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
+class UserMailer < ApplicationMailer
 
 
 
   def account_activation(user)
     @user = user
     mail to: user.email, subject: "User Activation", from: 'leonard.morrison@outlook.com'
-
   end
+
+
 end
