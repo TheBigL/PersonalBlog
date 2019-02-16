0246ff49905a887d3a8b2c3c5e64e7e3c2868e91
TheBigL <lmohamed1@studentmail.nait.ca>
2019-02-16 00:15:17 -0700

    Updated Gems

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,12 +86,18 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+  config.force_ssl = true
+
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
+
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :sendmail #:smtp
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
