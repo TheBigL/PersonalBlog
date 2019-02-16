cc52c059233d413897bbd423b7bf77c89177642a
TheBigL <lmohamed1@studentmail.nait.ca>
2019-02-15 23:26:52 -0700

    Remove Dependency  on Application Mailer 2

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   config.mailer = 'UserMailer'
 
   # Configure the parent class responsible to send e-mails.
-  config.parent_mailer = 'Devise::Mailer'
+  config.parent_mailer = 'ApplicationMailer'
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
