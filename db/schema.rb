9d03f8a532361ca9d68ffd6c860891e0d94c264d
TheBigL <lmohamed1@studentmail.nait.ca>
2019-02-24 22:41:14 -0700

    Added Tags to the Database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_04_195544) do
+ActiveRecord::Schema.define(version: 2019_02_25_041921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,12 @@ ActiveRecord::Schema.define(version: 2019_02_04_195544) do
   create_table "roles", force: :cascade do |t|
     t.integer "role_id"
     t.string "roleName"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
