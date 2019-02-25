1f6f713269111440598c85cf473c28beff838d82
TheBigL <lmohamed1@studentmail.nait.ca>
2019-02-25 16:51:22 -0700

    Added more about myself in About ME

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,6 +96,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
 
+  
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
