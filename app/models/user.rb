cb781cb8b02933da6f978c74b13d2bbed7cfc067
TheBigL <lmohamed1@studentmail.nait.ca>
2019-03-01 16:40:59 -0700

    Removing Trackable from the User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
   belongs_to :role
   before_save :setup_role
 
