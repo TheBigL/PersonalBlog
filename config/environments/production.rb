4e012aea7ef7aa941a8452ae8d1e65d09714fafb
TheBigL <lmohamed1@studentmail.nait.ca>
2019-03-02 15:25:12 -0700

    Enabling SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -70,7 +70,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -95,15 +95,6 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
-
-  config.action_mailer.default_url_options = {:host => 'personalblog.leonardmorrison.net'}
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  :address => "142.93.154.185",
-  :port    => 25,
-  :domain  => 'yourdomain.com'
-  }
-
 
 
   # Do not dump schema after migrations.
