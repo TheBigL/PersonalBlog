34e0d22a3a148bdac89379a1a3854c56d282ffd0
LM <lmohamed1@studentmail.nait.ca>
2024-08-29 12:03:20 -0600

    Fixed the Sample Page, started working on the User page

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
 # Create your views here.
-def members (request):
+def posts (request):
     template = loader.get_template('homepage.html')
     return HttpResponse(template.render())
