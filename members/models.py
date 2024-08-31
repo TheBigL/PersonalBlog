26ce12147358827b2f4b46e719de98882029aef0
LM <lmohamed1@studentmail.nait.ca>
2024-08-31 02:18:28 -0600

    Rebuilt the User file

--- members/models.py
+++ members/models.py
@@ -1,34 +1,70 @@
-from typing import Any
-from django.contrib.auth.models import User, UserManager
+from django.contrib.auth.models import User, UserManager, BaseUserManager, AbstractBaseUser
 from django.db import models
-from django.utils import timezone
 
-# Create your models here.
-class Member(UserManager):
-    def _create_user(self, email, password, **extra_fields):
+
+
+# Create a new User
+
+# Create a superuser
+
+class MemberManager(BaseUserManager):
+
+    def create_user(self, email, username, password=None):
         if not email:
-            raise ValueError("You've failed to provide your email")
-        
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+            raise ValueError("You must have an email address in order to register")
+        if not username:
+            raise ValueError("You must have a username")
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+        )
         user.set_password(password)
-        user.save(using=self.db)
-
+        user.save(using=self._db)
         return user
     
-    def create_user(self, email=None, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
-        return self.create_user(email, password, **extra_fields)
-    
-    def create_superuser(self, email=None, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        return self.create_user(email, password, **extra_fields)
-    
-    class Members(models.Model):
-        user = models.OneToOneField(User, on_delete=models.CASCADE)
-        is_contributor = models.BooleanField(default=False)
-        date_joined = models.DateTimeField(default=timezone.now)
+    def create_superuser(self, email, username, password):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            username=username,
+            password=password,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.is_contributor = True
+        user.save(using=self._db)
+        return user
 
+def get_profile_image_filepath(self, filename):
+    return f'profile_images/{str(self.pk)}/{"profile_image.png"}'
 
+# Create your models here.
+class Member(AbstractBaseUser):
+    email = models.EmailField(verbose_name="email", max_length=60, unique=True)
+    username = models.CharField(max_length=45, unique=True)
+    date_joined = models.DateTimeField(verbose_name="date joined", auto_now_add=True)
+    last_login = models.DateTimeField(verbose_name="last login", auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    profile_image = models.ImageField(max_length=255, upload_to='images/pfps/', null=True, blank=True)
+    is_contributor = models.BooleanField(default=False)
+    hide_email = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
+    
+    def get_profile_image_filename(self):
+        return str(self.profile_image)[str(self.profile_image).index(f'profile_images/{self.pk}/'):]
+    
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+    
+    def has_module_perms(self, app_label):
+        return True
+
+    
--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -35,17 +35,20 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = "members.Member"
 
 # Application definition
 
 INSTALLED_APPS = [
+    'posts',
+    'members',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'posts'
+
 
 ]
 
