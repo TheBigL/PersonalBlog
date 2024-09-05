4c6b4f600a0ddced480ee5061f53d64c98a2feb9
LM <lmohamed1@studentmail.nait.ca>
2024-09-05 08:49:33 -0600

    Created two more test for the User model

--- members/tests.py
+++ members/tests.py
@@ -6,12 +6,22 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 @pytest.fixture()
-def user_1(db):
+def basic_user(db):
     return User.objects.create_user('user@test.com', 'test')
 
+
 @pytest.mark.django_db
-def test_set_check_password_fail(user_1):
-    user_1.set_password("password")
-    assert user_1.check_password("wrong") is False
+def test_if_user_exists(basic_user):
+    assert User.objects.count() == 1
 
 
+@pytest.mark.django_db
+def test_set_check_password_fail(basic_user):
+    basic_user.set_password("password")
+    assert basic_user.check_password("wrong") is False
+
+
+@pytest.mark.django_db
+def test_set_check_password_success(basic_user):
+    basic_user.set_password("password")
+    assert basic_user.check_password("password") is True
