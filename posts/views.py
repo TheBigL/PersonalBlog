875daa2b04abd5a9628e1551408e03f0e9a55a62
LM <lmohamed1@studentmail.nait.ca>
2024-09-11 14:07:13 -0600

    Creating Blog Templates and operating the posts

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 from django.template import loader
 # Create your views here.
-def posts (request):
-    template = loader.get_template('homepage.html')
-    return HttpResponse(template.render())
+def HomeView (TemplateView):
+    return render(request, 'homepage.html', {})
