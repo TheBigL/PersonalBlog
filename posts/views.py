441849359bcb56b1a4f599a72c7a28da16221a11
LM <lmohamed1@studentmail.nait.ca>
2024-09-11 20:40:09 -0600

    Got the page working

--- posts/views.py
+++ posts/views.py
@@ -3,5 +3,5 @@ from django.views.generic import TemplateView
 from django.template import loader
 # Create your views here.
 def HomeView (TemplateView):
-    template = loader.get_template('homepage.html')
+    template = loader.get_template("homepage.html")
     return HttpResponse(template.render())
