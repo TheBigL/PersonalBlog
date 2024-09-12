d71d02302c9b0d5691beb1eb711d75f01308978a
LM <lmohamed1@studentmail.nait.ca>
2024-09-11 18:55:48 -0600

    Added 404 page and homepage

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import HomeView
+from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.HomeView, name='homepage'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -3,4 +3,5 @@ from django.views.generic import TemplateView
 from django.template import loader
 # Create your views here.
 def HomeView (TemplateView):
-    return render(request, 'homepage.html', {})
+    template = loader.get_template('homepage.html')
+    return HttpResponse(template.render())
