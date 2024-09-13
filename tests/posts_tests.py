eb945b22e0e39c8b3e79af0d845494435f20a7ea
LM <lmohamed1@studentmail.nait.ca>
2024-09-13 13:40:10 -0600

    Created the User fixture for the posts_tests.py

--- tests/posts_tests.py
+++ tests/posts_tests.py
@@ -4,3 +4,11 @@ from posts.models import Post
 
 # Create your tests here.
 User = get_user_model()
+
+#Fixture
+
+@pytest.fixture()
+def contributor(db):
+    contributor = User.objects.create_user('contributor@test.com', 'contributor')
+    contributor.is_contributor = True
+    yield contributor
