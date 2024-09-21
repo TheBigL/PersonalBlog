5735f1059386af971746e544715215baf8cb68e6
LM <lmohamed1@studentmail.nait.ca>
2024-09-21 11:02:09 -0600

    Adding EditView to the URLs

--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import include, re_path
-from views import AboutView, AddPostView, CreateView, DetailView, PostDetailView, PostListView
+from views import AboutView, AddPostView, CreateView, DetailView, PostDetailView, PostListView, PostEditView
 
 urlpatterns = [
     re_path(r'^$', PostListView.as_view, name="homeblog"),
     re_path(r'about', AboutView.as_view),
     re_path(r'post/<int:pk>', PostDetailView.as_view),
     re_path(r'add_post/', AddPostView.as_view, name="Add Post"),
-    re_path(r'post/edit/<int:pk>')
+    re_path(r'post/edit/<int:pk>', PostEditView.as_view)
     
 ]
