2918989b6b8adfe2efbb8b541292d67af95e3dea
LM <lmohamed1@studentmail.nait.ca>
2024-09-24 06:17:41 -0600

    Added AboutView back into the URLs

--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import re_path
-from .views import AddPostView, PostDetailView, PostListView, PostEditView
+from .views import AddPostView, PostDetailView, PostListView, PostEditView, AboutView
 
 urlpatterns = [
     re_path(r'^$', PostListView.as_view()),
     re_path(r'post/<int:pk>', PostDetailView.as_view()),
     re_path(r'add_post/', AddPostView.as_view()),
-    re_path(r'post/edit/<int:pk>', PostEditView.as_view())
-    
+    re_path(r'post/edit/<int:pk>', PostEditView.as_view()),
+    re_path(r'about/', AboutView.as_view())
     
 ]
