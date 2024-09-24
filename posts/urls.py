fe0e0d7da8a791911f83dad7ad33c3482807d405
LM <lmohamed1@studentmail.nait.ca>
2024-09-24 14:24:12 -0600

    Added slashes to the urls in Posts URL

--- posts/urls.py
+++ posts/urls.py
@@ -3,9 +3,9 @@ from .views import AddPostView, PostDetailView, PostListView, PostEditView, Abou
 
 urlpatterns = [
     re_path(r'^$', PostListView.as_view()),
-    re_path(r'post/<int:pk>', PostDetailView.as_view()),
+    re_path(r'post/<int:pk>/', PostDetailView.as_view()),
     re_path(r'add_post/', AddPostView.as_view()),
-    re_path(r'post/edit/<int:pk>', PostEditView.as_view()),
+    re_path(r'post/edit/<int:pk>/', PostEditView.as_view()),
     re_path(r'post/aboutme/', AboutView.as_view())
     
 ]
