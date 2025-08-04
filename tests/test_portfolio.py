f137c2ad772143f9282d591b34ab8ba7559792c8
LM <lmohamed1@studentmail.nait.ca>
2025-08-04 11:24:24 -0600

    Started built the Test Class for Portfolios

---
+++ tests/test_portfolio.py
@@ -0,0 +1,23 @@
+import pytest
+from django.urls import reverse, NoReverseMatch
+from django.test import Client
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
+from portfolio.models import Portfolio
+from members.models import Member
+
+
+User = get_user_model()
+
+
+@pytest.fixture
+def test_client():
+    """Provides a Django test client."""
+    return Client()
+
+class TestPortfolioPermissions:
+
+    @pytest.mark.django_db
+    def test_create_portoflio():
+        return None
