8e05359d70cd488fec03e36608e233030393078d
LM <lmohamed1@studentmail.nait.ca>
2025-11-26 09:26:18 -0700

    Creating the URLs and Views

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,3 +3,8 @@ from .models import Portfolio
 from django.shortcuts import render, redirect
 
 # Create your views here.
+class PortfolioListView(ListView):
+    model = Portfolio
+    template_name = 'portfolio.html'
+    context_object_name = 'portfolios'
+
