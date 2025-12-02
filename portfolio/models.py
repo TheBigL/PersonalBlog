2c16432351568d2981b048d922105ecafaf086e6
LM <lmohamed1@studentmail.nait.ca>
2025-12-02 16:41:45 -0700

    Adding link to the model

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,6 +5,7 @@ class Portfolio(models.Model):
     name = models.CharField(max_length=100, verbose_name="Portfolio Name")
     description = models.TextField(verbose_name="Portfolio Description")
     img = models.ImageField(upload_to='portfolio_images/', verbose_name="Portfolio Image")
+    link = models.URLField(max_length=200, verbose_name="Portfolio Link")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created At")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated At")
 
