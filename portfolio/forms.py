884aba49b628912ad699e155fd01235c58aecaa6
LM <lmohamed1@studentmail.nait.ca>
2025-12-02 12:42:36 -0700

    Reverting Changes Part 2

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -5,8 +5,6 @@ from .models import Portfolio
 class PortfolioForm(forms.ModelForm):
     title = forms.CharField(label="Title", max_length=150)
     content = forms.CharField()
-    image = forms.ImageField(required=False)
-    link = forms.URLField(required=False)
 
     class Meta:
         model = Portfolio
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,10 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Portfolio
 from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from django.core.exceptions import PermissionDenied
-from django.utils.decorators import method_decorator
 
 # Create your views here.
+class PortfolioListView(ListView):
+    model = Portfolio
+    template_name = 'portfolio.html'
+    context_object_name = 'portfolios'
+
