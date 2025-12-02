71f2e2279c89cb82cfd94750a3bec61188f4641a
LM <lmohamed1@studentmail.nait.ca>
2025-12-02 13:08:50 -0700

    Adding to the Portfolio Forms

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -5,6 +5,8 @@ from .models import Portfolio
 class PortfolioForm(forms.ModelForm):
     title = forms.CharField(label="Title", max_length=150)
     content = forms.CharField()
+    img = forms.ImageField(label="Portfolio Image", required=False)
+    link = forms.URLField(label="Portfolio Link", required=False)
 
     class Meta:
         model = Portfolio
