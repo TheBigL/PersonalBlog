fceb69188ecc0d0bb3139c24d61c6d936446a7a6
LM <lmohamed1@studentmail.nait.ca>
2025-12-05 11:33:09 -0700

    Added Delete Portfolio View

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,8 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Portfolio
+from .forms import PortfolioForm
 from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
@@ -20,7 +21,7 @@ class PortfolioDetailView(DetailView):
 @method_decorator(login_required, name='dispatch')
 class AddPortfolioView(CreateView):
     model = Portfolio
-    fields = ['name', 'description', 'img', 'link']
+    form_class = PortfolioForm
     template_name = 'portfolio/addportfolio.html'
     success_url = '/portfolio/'
 
@@ -37,8 +38,23 @@ class AddPortfolioView(CreateView):
     
 
 @method_decorator(login_required, name='dispatch')
-class UpdatePortfolioView(UpdateView):
+class EditPortfolioView(UpdateView):
     model = Portfolio
     fields = ['name', 'description', 'img', 'link']
-    template_name = 'portfolio/updateportfolio.html'
+    template_name = 'portfolio/edit_portfolio.html'
     success_url = '/portfolio/'
+
+    def get_queryset(self):
+        query_set = super().get_queryset()
+        return query_set.filter(created_by=self.request.user)
+    
+    def get_success_url(self):
+        return reverse('portfolio:portfolio_detail', kwargs={'pk': self.object.pk})
+
+@method_decorator(login_required, name='dispatch')
+class DeletePortfolioView(DeleteView):
+    model = Portfolio
+    template_name = 'portfolio/delete_portfolio.html'
+    success_url = reverse_lazy('portfolio:portfolio_list')
+
+    
