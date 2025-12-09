25c79e2b4ae0f45a56a55df5abbeabd6f7e52ee4
LM <lmohamed1@studentmail.nait.ca>
2025-12-08 20:23:40 -0700

    Added the URLs for Portfolios

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,2 +1,12 @@
 from django.urls import re_path
 from .views import PortfolioDetailView, PortfolioListView, AddPortfolioView, EditPortfolioView, DeletePortfolioView
+app_name = 'portfolio'
+
+
+urlpatterns = [
+    re_path(r'^$', PortfolioListView.as_view(), name="portfolio_list"),
+    re_path(r'^portfolio/add_portfolio/', AddPortfolioView.as_view(), name="add_portfolio"),
+    re_path(r'^portfolio/portfolio_detail/(?P<pk>\d+)/$', PortfolioDetailView.as_view(), name="portfolio_detail"),
+    re_path(r'^portfolio/edit_portfolio/(?P<pk>\d+)/$', EditPortfolioView.as_view(), name="edit_portfolio"),  
+    re_path(r'^portfolio/delete_portfolio/(?P<pk>\d+)/$', DeletePortfolioView.as_view(), name="delete_portfolio"),
+]
