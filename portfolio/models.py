00604e00e1160f9c84c034f64d3c0dbb76cebc2e
LM <lmohamed1@studentmail.nait.ca>
2025-12-15 16:05:04 -0700

    Added Portfolio model's migration to the database

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,11 +4,11 @@ from django.db import models
 class Portfolio(models.Model):
     name = models.CharField(max_length=100, verbose_name="Portfolio Name")
     description = models.TextField(verbose_name="Portfolio Description")
-    img = models.ImageField(upload_to='portfolio_images/', verbose_name="Portfolio Image")
-    link = models.URLField(max_length=200, verbose_name="Portfolio Link")
+    img = models.ImageField(upload_to='portfolio_images/', verbose_name="Portfolio Image", null=True)
+    link = models.URLField(max_length=200, verbose_name="Portfolio Link", null=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created At")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated At")
-
+    
     def __str__(self):
         return self.name
 
